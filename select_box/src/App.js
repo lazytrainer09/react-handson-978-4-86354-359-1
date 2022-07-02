2c73a8d17071f4bfa4d2297654096a334f34f55e
lazytrainer09 <wihan0927@gmail.com>
2022-07-02 16:35:45 +0900

    セレクトボックス

---
+++ select_box/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const values = [
+  { id: 1, item: "HTML" },
+  { id: 2, item: "CSS" },
+  { id: 3, item: "JavaScript" }
+];
+
+const SelectItems = values.map((value) => {
+  return (
+    <option value={value.item} key={value.id}>
+      {value.item}
+    </option>
+  );
+});
+
+const InputSelectBox = () => {
+  const [selectedValue, setSelectedValue] = useState(values[0]["item"]);
+
+  const handleChange = (e) => {
+    setSelectedValue(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <p>
+        現在選択されている値：
+        <b>{selectedValue}</b>
+      </p>
+
+      <select value={selectedValue} onChange={handleChange}>
+        {SelectItems}
+      </select>
+    </div>
+  );
+};
+
+export default function App() {
+  return <InputSelectBox />;
+}
