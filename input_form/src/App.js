f24ae77a9f6be7c0bf90563e56ea67c98713906a
lazytrainer09 <wihan0927@gmail.com>
2022-07-02 16:18:12 +0900

    入力フォーム

---
+++ input_form/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const InputText = () => {
+  const [inputTextValue, setInputTextValue] = useState("");
+
+  const [text, setText] = useState("JavaScript");
+
+  const handleChange = (e) => setInputTextValue(e.target.value);
+
+  const handleClick = () => {
+    setText(inputTextValue);
+    setInputTextValue("");
+  };
+
+  return (
+    <div className="App">
+      <h1>I love {text}!!</h1>
+      <input type="text" value={inputTextValue} onChange={handleChange} />
+      <input type="button" value="入力" onClick={handleClick} />
+    </div>
+  );
+};
+
+export default function App() {
+  return <InputText />;
+}
