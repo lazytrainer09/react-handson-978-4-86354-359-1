4673855d394573e81dca9065662365330c76942a
lazytrainer09 <wihan0927@gmail.com>
2022-07-03 18:53:30 +0900

    React.memo

---
+++ React_memo/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const CountResult = React.memo(({ text, countState }) => {
+  console.log(`${text}ボタンがクリックされました！`);
+  return (
+    <p>
+      {text}: {countState}
+    </p>
+  );
+});
+
+const Counter = () => {
+  const [countStateA, setCountStateA] = useState(0);
+  const [countStateB, setCountStateB] = useState(0);
+
+  const countIncrementA = () =>
+    setCountStateA((prevCountStateA) => prevCountStateA + 1);
+  const countIncrementB = () =>
+    setCountStateB((prevCountStateB) => prevCountStateB + 1);
+
+  return (
+    <>
+      <CountResult text="Aボタン" countState={countStateA} />
+      <CountResult text="Bボタン" countState={countStateB} />
+      <button onClick={countIncrementA}>Aボタン</button>
+      <button onClick={countIncrementB}>Bボタン</button>
+    </>
+  );
+};
+
+export default function App() {
+  return <Counter />;
+}
