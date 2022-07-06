1c7eb231effa8968175486d54544fa5d84c81b2f
lazytrainer09 <wihan0927@gmail.com>
2022-07-06 22:25:12 +0900

    TODOの取得

--- todo-app/src/components/App.js
+++ todo-app/src/components/App.js
@@ -1,6 +1,38 @@
+import React, {
+  useState,
+  useEffect
+} from "react";
+import axios from "axios";
+
+const todoDataUrl = "http://localhost:3100/todos";
+
 function App() {
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(todoDataUrl);
+      setTodoList(response.data);
+    };
+    fetchData();
+  }, []);
+
+  console.log("TODOリスト:", todoList);
+
   return (
-    <p>これからTODOアプリを実装します！</p>
+    <>
+      <h1>TODO進捗管理</h1>
+      <textarea />
+      <button>+ TODOを追加</button>
+      <h2>TODOリスト</h2>
+      <ul>
+        {todoList.map((todo) => (
+          <li key={todo.id}>
+            {todo.content}({todo.done ? "完了" : "未完了"})
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
