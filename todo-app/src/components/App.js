75482650cd707c70645232a7c5d30bcbeb655c73
lazytrainer09 <wihan0927@gmail.com>
2022-07-09 13:36:31 +0900

    Appコンポーネント

--- todo-app/src/components/App.js
+++ todo-app/src/components/App.js
@@ -1,4 +1,8 @@
 import React, { useRef } from "react";
+
+import { Container } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
+
 import { useTodo } from "../hooks/useTodo";
 
 import { TodoTitle } from "./TodoTitle";
@@ -30,11 +34,16 @@ function App() {
   });
 
   return (
-    <>
-      <TodoTitle title="TODO進捗管理" as="h1" />
-
+    <Container centerContent p={{ base: "4", md: "6" }} maxWidth="3xl">
+      <TodoTitle
+        title="TODO進捗管理"
+        as="h1"
+        fontSize={{ base: "2xl", md: "3xl" }}
+      />
       <TodoAdd
-        buttonText="+TODOを追加"
+        placeholder="ADD TODO"
+        leftIcon={<AddIcon />}
+        buttonText="TODOを追加"
         inputEl={inputEl}
         handleAddTodoListItem={handleAddTodoListItem}
       />
@@ -44,15 +53,17 @@ function App() {
         deleteTodoListItem={deleteTodoListItem}
         title="未完了TODOリスト"
         as="h2"
-        />
+        fontSize={{ base: "xl", md: "2xl" }}
+      />
       <TodoList
         todoList={completedList}
         toggleTodoListItemStatus={toggleTodoListItemStatus}
         deleteTodoListItem={deleteTodoListItem}
         title="完了TODOリスト"
         as="h2"
+        fontSize={{ base: "xl", md: "2xl" }}
       />
-    </>
+    </Container>
   );
 }
 
